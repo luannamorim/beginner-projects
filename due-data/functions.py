9e114c399b312d75929081e029c9d1a02ba162bb
luann <luanntech@gmail.com>
2023-10-09 20:19:47 -0400

    criacao modulo due-date

---
+++ due-data/functions.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+
+def today():
+    today = datetime.now()
+    return today
+
+
+def verify_date(date):
+    try:
+        date_format = datetime.strptime(date, "%d-%m-%Y")
+        return date_format
+    except:
+        raise Exception(
+            "Entrada inválida! Formato sugerido: D-M-Y. Exemplo: 01-09-2000")
+
+
+def verify_due(data_ref):
+    due_date = verify_date(date=data_ref)
+    if today() > due_date:
+        return "Data expirou..."
+    else:
+        return "Data não expirou..."
