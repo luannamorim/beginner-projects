0a26b6c84ec9080c1076126d330f3ba279a9cc95
luann <luanntech@gmail.com>
2023-10-16 22:42:44 -0400

    :sparkles: fear: select folder

--- qrcode-generator/main.py
+++ qrcode-generator/main.py
@@ -1,17 +1,19 @@
 import qrcode
 import tkinter as tk
 from tkinter import filedialog
+from datetime import datetime
 
 
 data = input('Link QRCODE: ')
-name = input('Name QRCODE: ')
+name = datetime.now()
+formatted_name = name.strftime("%Y%m%d-%H%M%S")
 
 root = tk.Tk()
 dirNome = filedialog.askdirectory(
     parent=root, initialdir="/", title='Select the folder')
 
 img = qrcode.make(data)
-file_path = f"{dirNome}/{name}.png"
+file_path = f"{dirNome}/{formatted_name}.png"
 img.save(file_path)
 
 print(f'QRCODE saved successfully at: {file_path}')
