f456776480925f16b433df8a2ab068870e4b597c
luann <luanntech@gmail.com>
2023-11-11 17:10:45 -0400

    new module

--- ws-airbnb/main.py
+++ ws-airbnb/main.py
@@ -1,14 +1,21 @@
-from bs4 import BeautifulSoup
-import requests
+from web_scraping import WebScraping
 
-class WebScraping:
-    def __init__(self, url):
-        self.url = url
 
-    def __get_html(self):
-        req = requests.get(self.url)
-        soup = BeautifulSoup(req.content, "html.parser")
-        return soup
-    
-    del __get_rooms(self):
-        return self.soup.find_all("div", class_ = "_1fdf")
+local = input("Destination: ")
+local = local.strip().replace(" ", "")
+guests = int(input("Number of guests: "))
+check_in = input("Check-in (YYYY-MM-DD): ")
+checkout = input("Chekout (YYYY-MM-DD): ")
+
+url = f"https://www.airbnb.com.br/s/{local}/homes?adults={guests}&checkin={check_in}&checkout={checkout}"
+
+web_scraping = WebScraping(url)
+all_room = web_scraping.pick_all_room()
+
+for airbnb in all_room:
+    print(f"\nLocal: {airbnb['Title']}")
+    print(f"Description: {airbnb['Subtitle']}")
+    print(f"Assessment: {airbnb['Assessment']}")
+    print(f"{airbnb['Total Price']}")
+
+print("\nLink: ", url)
---
+++ ws-airbnb/web_scraping.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class WebScraping:
+    def __init__(self, url):
+        self.url = url
+        self.soup = self.__get_html()
+
+    def __get_html(self):
+        req = requests.get(self.url)
+        soup = BeautifulSoup(req.content, "html.parser")
+        return soup
+
+    def __get_rooms(self):
+        return self.soup.find_all("div", class_="c4mnd7m")
+
+    def __get_subtitle(self, room):
+        return room.find("div", class_="g1qv1ctd c1v0rf5q dir dir-ltr").find("span", class_="t6mzqp7 dir dir-ltr").text
+
+    def __get_title(self, room):
+        return room.find("div", class_="t1jojoys dir dir-ltr").text
+
+    def __get_ass(self, room):
+        return room.find("div", class_="g1qv1ctd c1v0rf5q dir dir-ltr").find("span", class_="t1a9j9y7 r4a59j5 dir dir-ltr").find("span", class_="r1dxllyb dir dir-ltr").text
+
+    def __get_total(self, room):
+        return room.find("div", class_="_tt122m").text
+
+    def pick_all_room(self):
+        list_rooms = []
+        for room in self.__get_rooms():
+            room_info = {}
+
+            room_info["Title"] = self.__get_title(room)
+            room_info["Subtitle"] = self.__get_subtitle(room)
+            # room_info["Price per Night (R$)"] = self.__get_price(room)
+            room_info["Assessment"] = self.__get_ass(room)
+            room_info["Total Price"] = self.__get_total(room)
+
+            list_rooms.append(room_info)
+
+        return list_rooms
