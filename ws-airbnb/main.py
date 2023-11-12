09db92906005e8fff797da8e3c5fe499c71aec70
luann <luanntech@gmail.com>
2023-11-12 13:16:30 -0400

    updates

--- ws-airbnb/main.py
+++ ws-airbnb/main.py
@@ -12,10 +12,10 @@ url = f"https://www.airbnb.com.br/s/{local}/homes?adults={guests}&checkin={check
 web_scraping = WebScraping(url)
 all_room = web_scraping.pick_all_room()
 
+print("\nThe best Airbnb options:")
+
 for airbnb in all_room:
     print(f"\nLocal: {airbnb['Title']}")
     print(f"Description: {airbnb['Subtitle']}")
-    print(f"Assessment: {airbnb['Assessment']}")
-    print(f"{airbnb['Total Price']}")
-
-print("\nLink: ", url)
+    # print(f"Assessment: {airbnb['Assessment']}")
+    print(f"Price: {airbnb['Total Price']}")
--- ws-airbnb/web_scraping.py
+++ ws-airbnb/web_scraping.py
@@ -21,11 +21,14 @@ class WebScraping:
     def __get_title(self, room):
         return room.find("div", class_="t1jojoys dir dir-ltr").text
 
-    def __get_ass(self, room):
-        return room.find("div", class_="g1qv1ctd c1v0rf5q dir dir-ltr").find("span", class_="t1a9j9y7 r4a59j5 dir dir-ltr").find("span", class_="r1dxllyb dir dir-ltr").text
+    # def __get_ass(self, room):
+    #     return room.find("div", class_="g1qv1ctd c1v0rf5q dir dir-ltr").find("span", class_="t1a9j9y7 r4a59j5 dir dir-ltr").find("span", class_="r1dxllyb dir dir-ltr").text
 
     def __get_total(self, room):
-        return room.find("div", class_="_tt122m").text
+        try:
+            return room.find("div", class_="_tt122m").text
+        except:
+            return room.find("div", class_="_i5duul").find("span", class_="a8jt5op dir dir-ltr").text
 
     def pick_all_room(self):
         list_rooms = []
@@ -35,7 +38,7 @@ class WebScraping:
             room_info["Title"] = self.__get_title(room)
             room_info["Subtitle"] = self.__get_subtitle(room)
             # room_info["Price per Night (R$)"] = self.__get_price(room)
-            room_info["Assessment"] = self.__get_ass(room)
+            # room_info["Assessment"] = self.__get_ass(room)
             room_info["Total Price"] = self.__get_total(room)
 
             list_rooms.append(room_info)
